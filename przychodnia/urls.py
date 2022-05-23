3e1e8be197560ebb716d7a765798fda214cd6f48
lullusiek <lullusiek@gmail.com>
2022-05-23 20:50:13 +0200

    fix static paths

--- przychodnia/urls.py
+++ przychodnia/urls.py
@@ -1,7 +1,11 @@
+from django.conf import settings
+from django.contrib import admin
+from django.template.defaulttags import url
 from django.urls import path
+from django.conf.urls.static import static
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

9d50d88da466b9dc0861223a200e00d162ad2a3d
lullusiek <lullusiek@gmail.com>
2022-05-23 20:32:18 +0200

    rename to przychodnia

--- przychodnia/models.py
+++ przychodnia/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Owner(models.Model):
+    phone = models.CharField(max_length=9)
+    name = models.CharField(max_length=30)
+    address = models.CharField(max_length=50)
+
+
+class Animal(models.Model):
+    owner_key = models.ForeignKey(Owner, on_delete=models.CASCADE)
+    age = models.IntegerField()
+    name = models.CharField(max_length=30)
+
+    type = models.CharField(max_length=1, choices=[
+        ('d', 'dog'),
+        ('c', 'cat'),
+        ('h', 'hamster'),
+        ('g', 'guinea pig')
+    ])
+
+
+class Bill(models.Model):
+    owner_key = models.ForeignKey(Owner, on_delete=models.CASCADE)
+    value = models.FloatField()
+    date = models.DateField()
